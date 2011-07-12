86172cb69c86dcef4051e38d61174393317fcd10
Paul Lunow <paul@apeunit.com>
2011-07-12 17:16:44 +0200

    Added Callbacks, horizontally align and readme

---
+++ README.md
@@ -0,0 +1,16 @@
+LeanModal
+=========
+
+A super simple JQuery plugin for modal windows. Works with your CSS.
+
+Built for all the short dialogs, alerts, panels and such associated with an app, that you may want to handle in a modal window. Designed to handle hidden content, and doesn't apply any styles to the target element, other than for displaying and positioning.
+
+Original Version: http://leanmodal.finelysliced.com.au/
+
+
+Extended Version
+----------------
+
+* center the overlay horizontally with option {top: -1}
+* added onOpen and onClose callbacks
+* readme with markdown syntax
--- jquery.leanModal.js
+++ jquery.leanModal.js
@@ -5,63 +5,41 @@
         leanModal: function(options) {
  
             var defaults = {
-                top: 100,
-                overlay: 0.5
+                top: -1,
+                overlay: 0.5,
+                onOpen: function() {},
+                onClose: function() {}
             }
                  
-            options =  $.extend(defaults, options);
- 
+            var o = $.extend(defaults, options);
+
             return this.each(function() {
-            
-                var o = options;
-               
                 $(this).click(function(e) {
-              
-          	  	var overlay = $("<div id='lean_overlay'></div>");
-              
-              	var modal_id = $(this).attr("href");
-
-				$("body").append(overlay);
-                
-				$("#lean_overlay").click(function() { 
-                     close_modal(modal_id);                    
-                });
-                         	
-              	var modal_height = $(modal_id).outerHeight();
-        	  	var modal_width = $(modal_id).outerWidth();
-
-        		$('#lean_overlay').css({ 'display' : 'block', opacity : 0 });
-
-        		$('#lean_overlay').fadeTo(200,o.overlay);
-
-        		$(modal_id).css({ 
-        		
-        			'display' : 'block',
-        			'position' : 'fixed',
-        			'opacity' : 0,
-        			'z-index': 11000,
-        			'left' : 50 + '%',
-        			'margin-left' : -(modal_width/2) + "px",
-        			'top' : o.top + "px"
-        		
-        		});
-
-        		$(modal_id).fadeTo(200,1);
-
-                e.preventDefault();
-                		
-              	});
-             
-            });
-
-			function close_modal(modal_id){
-
-        		$("#lean_overlay").fadeOut(200);
-
-        		$(modal_id).css({ 'display' : 'none' });
-			
-			}
-    
+	          	  	var overlay = $("<div id='lean_overlay'></div>");
+	              	var modal_id = $(this).attr("href");
+					$("body").append(overlay);
+					$("#lean_overlay").click(function() { 
+	                    $("#lean_overlay").fadeOut(200, o.onClose);
+		        		$(modal_id).css({ 'display' : 'none' });                    
+	                });	
+	              	var modal_height = $(modal_id).outerHeight();
+	        	  	var modal_width = $(modal_id).outerWidth();
+	        		$('#lean_overlay').css({ 'display' : 'block', opacity : 0 });
+	        		$('#lean_overlay').fadeTo(200, o.overlay);
+	        		$(modal_id).css({
+	        			'display': 'block',
+	        			'position': 'fixed',
+	        			'opacity': 0,
+	        			'z-index': 11000,
+	        			'left': 50 + '%',
+	        			'margin-left': -(modal_width/2) + 'px',
+	        			'top': (o.top < 0 ? '50%' : o.top + 'px'),
+	        			'margin-top': (o.top < 0 ? -(modal_height/2)+'px' : 0)
+	        		});
+	        		$(modal_id).fadeTo(200, 1, o.onOpen);
+	                e.preventDefault();
+             	});
+            });    
         }
     });
      
